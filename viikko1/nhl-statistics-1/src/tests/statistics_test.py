20b788a7027d3c53de21f94ded5c8c92f5c3edf6
Paavo Nätti <paavo.natti@gmail.com>
2022-11-03 19:08:30 +0200

    add first week

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,34 @@
+import unittest
+from statistics import Statistics
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        self.statistics = Statistics(PlayerReaderStub())
+
+    def test_statistics_finds_right_player(self):
+        player = self.statistics.search("Kurri")
+        self.assertEqual(player.name, "Kurri")
+
+    def test_statistics_returns_none_if_no_player(self):
+        self.assertEqual(self.statistics.search("Koivu"), None)
+
+    def test_statistics_finds_right_players_for_team(self):
+        players = self.statistics.team("EDM")
+        self.assertEqual(players[1].name, "Kurri")
+
+    def test_find_top_players(self):
+        players = self.statistics.top(2)
+        self.assertEqual(players[0].name, "Gretzky")
+        self.assertEqual(players[1].name, "Lemieux")
