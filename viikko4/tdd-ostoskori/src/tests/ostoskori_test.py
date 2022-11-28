756933df5d7487b7842b284f4c83c71862585ef8
Paavo Nätti <paavo.natti@gmail.com>
2022-11-28 19:38:33 +0200

    after two same items only one shopping

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -74,3 +74,11 @@ class TestOstoskori(unittest.TestCase):
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 2)
 
+    def test_kahden_saman_tuotteen_jalkeen_yksi_ostos(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
+
