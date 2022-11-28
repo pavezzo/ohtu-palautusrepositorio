0fcfaa1c0add16aa501340ae0ea1048091016228
Paavo Nätti <paavo.natti@gmail.com>
2022-11-28 19:32:14 +0200

    returns list of shoppings

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -5,28 +5,28 @@ from functools import reduce
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.ostokset = []
+        self._ostokset = []
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return reduce(lambda a, b: a+b.lukumaara(), self.ostokset, 0)
+        return reduce(lambda a, b: a+b.lukumaara(), self._ostokset, 0)
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        return reduce(lambda a, b: a+b.hinta(), self.ostokset, 0)
+        return reduce(lambda a, b: a+b.hinta(), self._ostokset, 0)
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
         loyty = False
-        for i, x in enumerate(self.ostokset):
+        for i, x in enumerate(self._ostokset):
             if x.tuotteen_nimi() == lisattava.nimi():
                 x.muuta_lukumaaraa(1)
                 loyty = True
                 break
         if not loyty:
-            self.ostokset.append(Ostos(lisattava))
+            self._ostokset.append(Ostos(lisattava))
                 
 
 
@@ -39,6 +39,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
+        return self._ostokset
