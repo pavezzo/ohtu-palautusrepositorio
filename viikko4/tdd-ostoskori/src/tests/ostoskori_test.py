c3c8ae7b541c330e0911be3129569555c9f936e5
Paavo Nätti <paavo.natti@gmail.com>
2022-11-28 19:22:30 +0200

    after two items the price is right

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -27,3 +27,11 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(voi)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_tuotteiden_summa(self):
+        maito = Tuote("Maito", 3)
+        voi = Tuote("Voi", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(voi)
+
+        self.assertEqual(self.kori.hinta(), 8)
