d120d3aa346ec2d7e3672c59270190e34c6d5eb2
Paavo Nätti <paavo.natti@gmail.com>
2022-12-11 15:22:16 +0200

    exercises 2-5

---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,20 @@
+from matchers import *
+
+class QueryBuilder:
+    def __init__(self, matcher = All()):
+        self._matcher = matcher
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
+
+    def oneOf(self, matcher1, matcher2):
+        return QueryBuilder(Or(matcher1, matcher2))
+
+    def build(self):
+        return self._matcher
